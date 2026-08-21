2a78eb590887a7bcabf94bcb4b768cdbb1f3915e
Tejaskarakoti <karakotitejas@gmail.com>
2026-08-21 11:52:24 +0530

    Time: 0 ms (100%), Space: 21.6 MB (94.1%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int ans=0; 
+
+        for(int i=0 ;i<=nums.size(); i++){
+            ans^=i;
+        }
+
+        for(int it:nums){
+            ans^=it;
+        }
+        return ans;
+    }
+};
