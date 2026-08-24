4970e09958e226dc6bc95b796c6d9c09deef9590
Tejaskarakoti <karakotitejas@gmail.com>
2026-08-24 21:37:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (94.3%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        string s="";
-        int count = 0;
-
-        while(count < 32) {
-       int rem = n % 2;
-            s +=char(rem +'0');
-            n = n/ 2;
-            count++;
-        }
         int ans = 0;
 
-        for(char c : s) {
-            ans = ans * 2 + (c - '0');
+        for(int i = 0; i < 32; i++) {
+            ans = ans *2+ n% 2;
+            n/=2;
         }
 
         return ans;
